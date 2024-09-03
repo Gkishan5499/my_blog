fab0434357f702e4b5b657fc378a4688ac13b8dc
omprakash30 <oy45833@gmail.com>
2024-09-03 11:26:12 +0530

    created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,34 @@
 
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+
+
+
+
 
 function App() {
   
-
   return (
     <>
-      <h1 className='text-[50px] text-red-500'>Hello</h1>
-        
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/dashboard' element={<Dashboard/>}/>
+            <Route path='/projects' element={<Projects/>}/>
+            <Route path='/sign-in' element={<Signin/>}/>
+            <Route path='/sign-up' element={<Signup/>}/>
+
+
+
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
